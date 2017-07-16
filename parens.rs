1b6ab68a2b34f324e8c33a52c81efb911d93c2be
Eshin Kunishima <ek@esh.ink>
2017-07-16 17:32:03 +0900

    Rust: simple concat

--- parens.rs
+++ parens.rs
@@ -1,36 +1,25 @@
 use std::env;
 use std::str;
 
-static mut SIZE: usize = 0;
-static mut STACK: [u8; 64] = [0x0; 64];
-
-unsafe fn _parens(left: usize, right: usize) {
+fn _parens(prefix: String, left: i32, right: i32) {
     if left == 0 && right == 0 {
-        println!("{}", str::from_utf8(&STACK).unwrap());
+        println!("{}", prefix);
     }
 
     if left > 0 {
-        STACK[SIZE - left - right] = b'(';
-        _parens(left - 1, right);
+        _parens(prefix.clone() + "(", left - 1, right);
     }
 
     if right > left {
-        STACK[SIZE - left - right] = b')';
-        _parens(left, right - 1);
+        _parens(prefix.clone() + ")", left, right - 1);
     }
 }
 
-fn parens(n: usize) {
-    unsafe {
-        SIZE = 2 * n;
-        if STACK.len() < SIZE {
-            panic!();
-        }
-        _parens(n, n);
-    }
+fn parens(n: i32) {
+    _parens("".to_string(), n, n);
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    parens(args[1].parse::<usize>().unwrap());
+    parens(args[1].parse::<i32>().unwrap());
 }
